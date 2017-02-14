8cd49eec4e1b81e17b0463c2459239839bf93bc3
Eli Dribben <YuBh8Tn@users.noreply.github.com>
2017-02-14 09:06:44 -0800

    Delete bgrandom.js

--- bgrandom.js
+++
@@ -1,7 +0,0 @@
-var totalCount = 6;
-function ChangeIt() 
-{
-var num = Math.ceil( Math.random() * totalCount );
-document.body.background = 'images/'+num+'.jpg';
-document.body.style.backgroundRepeat = "no-repeat";
-}
